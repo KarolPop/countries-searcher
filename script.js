dcc1c3af95fa247e94c30e2054a14d1166b1dbe9
Karol <karolpoplawski@gmail.com>
2017-01-02 16:05:34 +0100

    changed script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var url = 'https://restcountries.eu/rest/v1/name/'; 
+//var urlWithCapitals = 'https://restcountries.eu/rest/v1/capital/'; 
+var countriesList = $('#countries');
+$('#search').click(searchCountries);
+
+$('#country-name').keydown(function(event){
+	if(event.keyCode == 13){
+		 //$('#search').click();
+		searchCountries();
+	}
+});
+
+function searchCountries() { 
+	var countryName = $('#country-name').val(); 
+	if(!countryName.length) {
+		countryName = 'Poland'; 	
+}
+
+$.ajax({ 
+		url: url + countryName, 
+		method: 'GET', 
+		success: showCountriesList 
+	}); 
+}
+
+//Countries list search result  
+function showCountriesList(resp) { 
+	countriesList.empty(); 
+	resp.forEach(function(item) { 
+		$('<li>').text(item.name + ' - ' + item.region + ' - ' + item.capital).appendTo(countriesList);
+	}); 
+}
